a2afc3a16207c69d6fd23d6b6b04bc862defb98e
Tianlin Lu <txl355@case.edu>
2019-08-22 12:01:44 -0700

    add stock listing and remove functions and users details

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
